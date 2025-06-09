fb676eeeebab1311b021032d39097d783fe00a22
Marcelo Valle <mvallebr@gmail.com>
2025-06-09 19:48:33 +0100

    Refactor

--- run_test_vm.sh
+++ run_test_vm.sh
@@ -3,6 +3,8 @@ set -e
 set -u
 set -x
 
+VM_NAME="saas-server"
+
 RESTART=0
 if [ "${1:-}" = "--restart" ]; then
     RESTART=1
@@ -25,7 +27,7 @@ fi
 # Delete existing instance only if --restart is passed
 if [ "$RESTART" -eq 1 ] || [ "$IS_SAAS_SERVER_VM_RUNNING" -eq 0 ]; then
     if [ "$IS_SAAS_SERVER_VM_RUNNING" -eq 1 ]; then
-        multipass delete saas-server
+        multipass delete ${VM_NAME}
         multipass purge
     fi
     # Export your public key as an environment variable
@@ -34,16 +36,11 @@ if [ "$RESTART" -eq 1 ] || [ "$IS_SAAS_SERVER_VM_RUNNING" -eq 0 ]; then
     # Generate cloud-init.yaml with your public key
     envsubst <cloud-init.yaml >cloud-init.generated.yaml
     # Launch VM
-    multipass launch --name saas-server --cpus 2 --memory 4G --disk 25G 24.04 --cloud-init cloud-init.generated.yaml
-
-    # Inject your public SSH key into the VM for Ansible access
-    # multipass exec saas-server -- mkdir -p /home/ubuntu/.ssh
-    # multipass exec saas-server -- bash -c "echo '$(cat ~/.ssh/id_rsa.pub)' >> /home/ubuntu/.ssh/authorized_keys"
-    # multipass exec saas-server -- chown ubuntu:ubuntu /home/ubuntu/.ssh/authorized_keys
+    multipass launch --name ${VM_NAME} --cpus 2 --memory 4G --disk 25G 24.04 --cloud-init cloud-init.generated.yaml
 fi
 
 # Get IP
-export HOST_PUBLIC_IP=$(echo "$MP_LIST_OUTPUT" | grep saas-server | awk '{print $3}')
+export HOST_PUBLIC_IP=$(echo "$MP_LIST_OUTPUT" | grep ${VM_NAME} | awk '{print $3}')
 envsubst <inventory.sample.yml >inventory.yml
 
 # Install Ansible external collections
