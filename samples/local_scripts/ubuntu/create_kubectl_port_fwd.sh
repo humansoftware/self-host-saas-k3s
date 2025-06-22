57fcbde56ddae7395d6ffb71782bee42ff5c1ea3
Marcelo Valle <mvallebr@gmail.com>
2025-06-22 21:43:58 +0100

    Use correct admin port for mailu

--- samples/local_scripts/ubuntu/create_kubectl_port_fwd.sh
+++ samples/local_scripts/ubuntu/create_kubectl_port_fwd.sh
@@ -35,7 +35,7 @@ kubectl -n flux-system port-forward svc/weave-gitops 9001:9001 &
 echo "Flux UI: http://localhost:9001 (admin/<your password>)"
 
 # Forward Mailu Admin UI
-kubectl -n mailu port-forward svc/mailu-admin 8082:80 &
+kubectl -n mailu port-forward svc/mailu-admin 8082:8080 &
 echo "Mailu Admin: http://localhost:8082 (admin/<your password>)"
 
 # Wait for background jobs
